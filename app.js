dbe88e8ffce0ae84e152db3cb5a26c792b4db1e9
Nasseima <lindsaynasseima@gmail.com>
2024-06-24 22:07:43 -0400

    made necessary changes

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 //  Part one: humble Beginnings
-
-const adventurer = {
-    name: "Robin",
-    health: 10,
-    inventory: ["sword", "potion", "artifact"]
-    }
-    
+   
+    const adventurer = {
+        name: "Robin",
+        health: 10,
+        inventory: ["sword", "potion", "artifact"],
+        companion: {
+            name: "Leo",
+            type: "Cat"
+        }
+        }
+        for (let item of adventurer.inventory) {
+            console.log(item);
+        }
