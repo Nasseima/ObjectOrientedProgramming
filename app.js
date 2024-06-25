9f433909b3304f5ada518a2aecc9595d8d099a01
Nasseima <lindsaynasseima@gmail.com>
2024-06-24 22:15:26 -0400

    finished part one

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 //  Part one: humble Beginnings
-   
-    const adventurer = {
-        name: "Robin",
-        health: 10,
-        inventory: ["sword", "potion", "artifact"],
-        companion: {
-            name: "Leo",
-            type: "Cat"
-        }
-        }
-        for (let item of adventurer.inventory) {
+
+        const adventurer = {
+            name: "Robin",
+            health: 10,
+            inventory: ["sword", "potion", "artifact"],
+            companion: {
+              name: "Leo",
+              type: "Cat",
+              companion: {
+                name: "Frank",
+                type: "Flea",
+                belongings: ["small hat", "sunglasses"]
+              }
+            },
+            roll(mod = 0) {
+              const result = Math.floor(Math.random() * 20) + 1 + mod;
+              console.log(`${this.name} rolled a ${result}.`);
+            }
+          };
+          for (let item of adventurer.inventory) {
             console.log(item);
         }
+          console.log(adventurer);
+
+          adventurer.roll();
+          adventurer.roll();
+          adventurer.roll(-5);
